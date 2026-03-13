b1746a6a36ef2d37f92f56c857920915d8186c3f
Emma Sudo <183161889+emmasudo@users.noreply.github.com>
2026-03-12 20:42:14 -0700

    new memfunc.wasm

--- memcpy/e-memfunc.c
+++ memcpy/e-memfunc.c
@@ -6,16 +6,16 @@ SEC("tp/syscalls/sys_enter_execve")
 int detect_execve() {
   bpf_printk("%s\n", "execve called");
   bpf_printk("starting benchmarks!\n");
-  __u64 n = 1000;
+  __u64 n = 1000000;
   __u64 test = 0;
   __u64 start = bpf_ktime_get_ns();
   for (int i = 0; i < n; i++) {
     //memfunc(2);
-    char src[200];
-    char dst[200];
-    __builtin_memset(src, 123, 200);
-    __builtin_memcpy(dst, src, 200);
-    test = dst[i % 200];
+    char src[500];
+    char dst[500];
+    __builtin_memset(src, 123, 500);
+    __builtin_memcpy(dst, src, 500);
+    test = dst[i % 500];
   }
   __u64 total = bpf_ktime_get_ns() - start;
   bpf_printk("finished benchmarks! it took %d nanoseconds to do %d iterations\n", total, n);
--- memcpy/memfunc.c
+++ memcpy/memfunc.c
@@ -1,7 +1,7 @@
 char memfunc(int n) {
-    char src[20000];
-    char dst[20000];
-    __builtin_memset(src, 123, 20000);
-    __builtin_memcpy(dst, src, 20000);
+    char src[500];
+    char dst[500];
+    __builtin_memset(src, 123, 500);
+    __builtin_memcpy(dst, src, 500);
     return dst[n];
 }
